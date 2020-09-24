949fa115623df43e992d27484e38b789e3cf4bad
WhoIsRogerCreus <creus99@gmail.com>
2020-09-24 18:34:54 +0200

    try build

---
+++ CBI - UNITY/CBI/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class CameraManager : MonoBehaviour
+{
+    bool camAvailable;
+    WebCamTexture webCamTexture;
+    Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+
+    void Start()
+    {
+        defaultBackground = background.texture;
+        webCamTexture = new WebCamTexture();
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("NO CAM");
+            camAvailable = false;
+            return;
+        }
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (!devices[i].isFrontFacing)
+            {
+                webCamTexture = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+        if (webCamTexture == null)
+        {
+            return;
+        }
+
+        webCamTexture.Play();
+        background.texture = webCamTexture;
+
+        camAvailable = true;
+    }
+
+    private void Update()
+    {
+        if (!camAvailable)
+            return;
+
+        float ratio = (float)webCamTexture.width / (float)webCamTexture.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY = webCamTexture.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -webCamTexture.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+
+    }
+}
---
+++ CBI - UNITY/CBI/Assets/Scripts/ModelManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Barracuda;
+
+
+public class ModelManager : MonoBehaviour
+{
+
+    public NNModel modelSource;
+
+    private IWorker worker;
+    private Model model;
+
+    public Texture2D texture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        model = ModelLoader.Load(modelSource, verbose:false);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model, verbose:false);
+        FeedModel(null, texture);
+
+
+        foreach (var layer in model.layers)
+            Debug.Log(layer.name + " does " + layer.type);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Tensor ImageToTensor(Texture2D t)
+    {
+        //var tensor = new Tensor(batchCount, height, width, channelCount);
+
+        var channelCount = 3; // you can treat input pixels as 1 (grayscale), 3 (color) or 4 (color with alpha) channels
+        var tensor = new Tensor(t, channelCount);
+
+        return tensor;
+    }
+
+    void FeedModel(Dictionary<string, Tensor> inputs, Texture2D t)
+    {
+
+        worker.Execute(ImageToTensor(t));
+        var O = worker.PeekOutput();
+        Debug.Log(O[0, 0, 0, 1]);
+        FreeMemory(O);
+    }
+
+    void FreeMemory(Tensor outputs)
+    {
+        outputs.Dispose();
+        worker.Dispose();
+    }
+}
