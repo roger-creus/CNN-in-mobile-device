10af7076429a4d14f01daae5f8ba5ab4976ffe8b
yuyecreus <creus99@gmail.com>
2020-10-02 09:27:00 +0200

    Decent PoW

--- CBI - UNITY/CBI/Assets/Scripts/CameraManager.cs
+++ CBI - UNITY/CBI/Assets/Scripts/CameraManager.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
+using System;
 
 public class CameraManager : MonoBehaviour
 {
 
+
     private bool camAvailable;
     private WebCamTexture webCamTexture;
     private Texture defaultBackground;
+
+    public ModelManager mm;
 
     public RawImage background;
     public AspectRatioFitter fit;
     public bool frontFacing;
 
     public Text camText;
+
+    float ratio, scaleY;
+    int orient;
     // Use this for initialization
     void Start()
     {
@@ -31,13 +38,16 @@ public class CameraManager : MonoBehaviour
 
             if (curr.isFrontFacing == frontFacing)
             {
-                webCamTexture = new WebCamTexture(curr.name, Screen.width, Screen.height);
+                webCamTexture = new WebCamTexture();
             }
         }
 
         if (webCamTexture == null)
             camText.gameObject.SetActive(true);
 
+        Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        Application.targetFrameRate = 300;
+        webCamTexture.requestedFPS = 600;
 
         webCamTexture.Play(); // Start the camera
         background.texture = webCamTexture; // Set the texture
@@ -50,33 +60,44 @@ public class CameraManager : MonoBehaviour
     {
        
 
-        float ratio = (float)webCamTexture.width / (float)webCamTexture.height;
+        ratio = (float)webCamTexture.width / (float)webCamTexture.height;
         fit.aspectRatio = ratio; // Set the aspect ratio
 
-        float scaleY = webCamTexture.videoVerticallyMirrored ? -1f : 1f; // Find if the camera is mirrored or not
+        scaleY = webCamTexture.videoVerticallyMirrored ? -1f : 1f; // Find if the camera is mirrored or not
         background.rectTransform.localScale = new Vector3(1f, scaleY, 1f); // Swap the mirrored camera
 
-        int orient = -webCamTexture.videoRotationAngle;
+        orient = -webCamTexture.videoRotationAngle;
         background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
     }
 
-    public void TakePicture()
+    public void Pict()
+    {
+        StartCoroutine("TakePicture");
+    }
+
+    public IEnumerator TakePicture()
     {
 
-        Debug.Log("HI");
+        yield return new WaitForEndOfFrame();
+
         Texture2D PhotoTaken = new Texture2D(webCamTexture.width, webCamTexture.height);
         PhotoTaken.SetPixels(webCamTexture.GetPixels());
         PhotoTaken.Apply();
 
         background.texture = PhotoTaken;
-        
+
+
+        Tuple<double[], double[]> result = mm.FeedModel(PhotoTaken);
+        mm.SetPredictionTexts(result.Item1, result.Item2);
 
         camAvailable = false;
+
     }
 
     public void BackToCam()
     {
         background.texture = webCamTexture;
+        mm.predictionPanel.SetActive(false);
         camAvailable = true;
     }
 
